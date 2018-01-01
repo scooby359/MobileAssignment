075bf0e5c8686346d16cbc07281e695480ed100c
scooby359 <scooby359@gmail.com>
2018-01-01 11:12:49 +0000

    Current weather request working and displays on home - need to implement on country screen - need to sort home screen to show faves and buttons to lists - bug fixes need doing

--- app/src/main/java/net/cwalton/mobileassignment/FragmentHome.java
+++ app/src/main/java/net/cwalton/mobileassignment/FragmentHome.java
@@ -8,13 +8,18 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -40,6 +45,15 @@ public class FragmentHome extends Fragment {
 
     private static final String LOG_TAG = "FragmentHome";
     private Context mContext;
+    private ConstraintLayout ml_weather_loading;
+    private ConstraintLayout ml_weather_loc_error;
+    private ConstraintLayout ml_data_error;
+    private ConstraintLayout ml_weather_forecast;
+    private CardView cv_home_weather;
+    private ImageView miv_weather_icon;
+    private TextView mtv_weather_location;
+    private TextView mtv_weather_condition;
+    private TextView mtv_weather_temp;
 
     @Override
     public void onAttach(Context context) {
@@ -52,20 +66,25 @@ public class FragmentHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        cv_home_weather = (CardView) view.findViewById(R.id.cv_home_loc_weather);
+        ml_weather_loading = (ConstraintLayout) view.findViewById(R.id.lay_home_weather_loading);
+        ml_weather_loc_error = (ConstraintLayout) view.findViewById(R.id.lay_home_weather_loc_error);
+        ml_data_error = (ConstraintLayout) view.findViewById(R.id.lay_home_weather_data_error);
+        ml_weather_forecast = (ConstraintLayout) view.findViewById(R.id.lay_home_weather_forecast);
+        miv_weather_icon = (ImageView) view.findViewById(R.id.iv_home_weather_icon);
+        mtv_weather_location = (TextView) view.findViewById(R.id.tv_home_weather_loc);
+        mtv_weather_condition = (TextView) view.findViewById(R.id.tv_home_weather_type);
+        mtv_weather_temp = (TextView) view.findViewById(R.id.tv_home_weather_temp);
+
         int haveLocPermission = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION);
-
-
-        TextView tv_status = (TextView) view.findViewById(R.id.tv_weather_status);
-
-        //todo remove this
-        if (haveLocPermission == 0)
-        {
-            tv_status.setText("Permission OK");
-        }else{
-            tv_status.setText("Permission denied");
-        }
-
         if (haveLocPermission == PackageManager.PERMISSION_GRANTED){
+
+            //display proper layout
+            ml_weather_loading.setVisibility(View.VISIBLE);
+            ml_weather_loc_error.setVisibility(View.GONE);
+            ml_data_error.setVisibility(View.GONE);
+            ml_weather_forecast.setVisibility(View.GONE);
 
             final Weather weatherHelper = new Weather();
             FusedLocationProviderClient mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(mContext);
@@ -75,31 +94,40 @@ public class FragmentHome extends Fragment {
                         public void onSuccess(Location location) {
                             if (location == null) {
                                 Log.d(LOG_TAG,"no location");
-                                        //handle no location known
-                                //adjust card displayed on screen
+                                ml_weather_loading.setVisibility(View.GONE);
+                                ml_weather_loc_error.setVisibility(View.VISIBLE);
                             }else{
                                 Log.d(LOG_TAG,"location found");
                                 Log.d(LOG_TAG, "Lat = " + location.getLatitude() + " loc = " + location.getLongitude());
-                                //handle weather request then update screen on result
-                                //need to do async?
 
                                 String url = weatherHelper.buildCurrentWeatherUrl(location);
 
+                                //todo - sort this section out!
                                 RequestQueue queue = Volley.newRequestQueue(getActivity());
                                 JsonObjectRequest weatherRequest = new JsonObjectRequest
                                         (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                                             @Override
                                             public void onResponse(JSONObject response) {
                                                 Log.d(LOG_TAG, "JSON Response = " + response);
+                                                ml_weather_loading.setVisibility(View.GONE);
+                                                ml_weather_forecast.setVisibility(View.VISIBLE);
+                                                weatherHelper.parseCurrentWeatherResponse(response);
+                                                mtv_weather_location.setText(weatherHelper.getmCityName());
+                                                mtv_weather_condition.setText(weatherHelper.getmCondition());
+                                                //todo - allow user to select farenheit
+                                                mtv_weather_temp.setText(weatherHelper.getmTemp()+"°c");
+                                                miv_weather_icon.setImageDrawable(getResources().getDrawable(weatherHelper.getmIcon()));
                                             }
                                         }, new Response.ErrorListener() {
                                             @Override
                                             public void onErrorResponse(VolleyError error) {
                                                 Log.d(LOG_TAG, "JSON Response Error returned");
+                                                ml_weather_loading.setVisibility(View.GONE);
+                                                ml_data_error.setVisibility(View.VISIBLE);
                                             }
                                         });
+                                //Todo - blocked request while fixing layout
                                 queue.add(weatherRequest);
-
 
                             }
                         }
@@ -107,10 +135,13 @@ public class FragmentHome extends Fragment {
         }else{
             Log.d(LOG_TAG, "Permission wasn't obtained so not requesting location");
                     //handle no loc permission - eg change display
+            cv_home_weather.setVisibility(View.GONE);
         }
 
         return view;
     }
+
+
 
 }
 
--- app/src/main/java/net/cwalton/mobileassignment/Weather.java
+++ app/src/main/java/net/cwalton/mobileassignment/Weather.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.location.*;
 import android.location.Location;
 import android.util.Log;
+import android.widget.Switch;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,45 +14,33 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import net.cwalton.mobileassignment.R;
+
 /**
  * Created by scoob on 31/12/2017.
+ * Ref - http://android-er.blogspot.co.uk/2015/10/android-query-current-weather-using.html
+ * For parsing JSON data
+ *
+ * https://materialdesignicons.com/
+ *For weather icons
  */
 
 public class Weather {
 
+    private static final String LOG_TAG = "Weather";
     private static String mCityName;
-    private static String mMinTemp;
-    private static String mMaxTemp;
+    private static String mTemp;
     private static String mCondition;
     private static int mIcon;
-    private static Context mContext;
-    private Location mLocation;
-
-
-
-    public static String getmCityName() {
-        return mCityName;
-    }
-
-    public static String getmMinTemp() {
-        return mMinTemp;
-    }
-
-    public static String getmMaxTemp() {
-        return mMaxTemp;
-    }
-
-    public static String getmCondition() {
-        return mCondition;
-    }
-
-    public static int getmIcon() {
-        return mIcon;
-    }
-
 
     private static final String API = "&APPID=";
     private static final String API_KEY = "4e58a95dd52b09ca48d9e6f3d362bc8c";
+
+    private static final String CENT = "&units=metric";
+    private static final String FAR = "&units=imperial";
 
     private static final String DAILY_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=";
     private static final String THREE_DAYS = "&cnt=3";
@@ -61,10 +50,9 @@ public class Weather {
     private static final String LAT = "lat=";
     private static final String LON = "&lon=";
 
-
-
     public String buildCurrentWeatherUrl(Location location){
-        String url = CURRENT_BASE_URL + LAT + location.getLatitude() + LON + location.getLongitude() + API + API_KEY;
+        String url = CURRENT_BASE_URL + LAT + location.getLatitude() + LON + location.getLongitude() + CENT + API + API_KEY;
+        Log.d("Weather","Url = " + url);
         return url;
     };
 
@@ -72,10 +60,122 @@ public class Weather {
         //todo - 3 day forecast for location
     }
 
-    public void parseJsonToWeather(){
-        //todo helper method to convert response to useable data
+    //http://android-er.blogspot.co.uk/2015/10/android-query-current-weather-using.html
+    public void parseCurrentWeatherResponse(JSONObject response){
+
+        String name = null;
+        String temp = null;
+        String condition = null;
+
+        String cod = jsonGetString(response, "cod");
+        if (cod!=null){
+            if (cod.equals("200")){
+
+                name = jsonGetString(response, "name");
+
+                JSONObject main = jsonGetJsonObject(response, "main");
+                if (main!=null){
+                    temp = jsonGetString(main, "temp");
+                }
+
+                JSONArray weather = jsonGetJsonArray(response, "weather");
+                if (weather!=null){
+
+                    JSONObject weatherObj = null;
+
+                    try {
+                        weatherObj = weather.getJSONObject(0);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (weatherObj!=null){
+                        condition = jsonGetString(weatherObj, "main");
+                    }
+
+                }
+
+                Log.d("Weather", "Name = " + name + ". Temp = " + temp + ". Condition = " + condition);
+
+                mCityName = name;
+                mTemp = temp;
+                mCondition = condition;
+                setIcon(condition);
+            }
+        }
     }
 
+    private String jsonGetString(JSONObject json, String param){
+        String response = null;
+        try {
+            response = json.getString(param);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    private JSONObject jsonGetJsonObject(JSONObject json, String param){
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = json.getJSONObject(param);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return  jsonObject;
+    }
+
+    private JSONArray jsonGetJsonArray(JSONObject json, String param){
+        JSONArray jsonArray = null;
+        try {
+            jsonArray = json.getJSONArray(param);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonArray;
+
+    }
+
+    private void setIcon(String condition){
+
+        if (condition.equals("Thunderstorm")){
+            mIcon = R.drawable.ic_weather_lightning;
+        }else if (condition.equals("Drizzle")){
+            mIcon = R.drawable.ic_weather_rainy;
+        }else if (condition.equals("Rain")){
+            mIcon = R.drawable.ic_weather_pouring;
+        }else if (condition.equals("Snow")){
+            mIcon = R.drawable.ic_weather_snowy;
+        }else if (condition.equals("Atmosphere")){
+            mIcon = R.drawable.ic_weather_fog;
+        }else if (condition.equals("Clear")){
+            mIcon = R.drawable.ic_weather_sunny;
+        }else if (condition.equals("Clouds")){
+            mIcon = R.drawable.ic_weather_cloudy;
+        }else if (condition.equals("Extreme")){
+            mIcon = R.drawable.ic_weather_lightning;
+        }else{
+            Log.d(LOG_TAG,"Condition unknown - defaulting icon");
+            mIcon = R.drawable.ic_weather_lightning;
+        }
+    }
+
+
+
+    public String getmCityName() {
+        return mCityName;
+    }
+    public String getmCondition() {
+        return mCondition;
+    }
+
+    public String getmTemp() {
+        return mTemp;
+    }
+
+    public int getmIcon() {
+        return mIcon;
+    }
 }
 
 
